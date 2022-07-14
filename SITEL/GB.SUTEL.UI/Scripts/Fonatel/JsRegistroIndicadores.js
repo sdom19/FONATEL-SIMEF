08980cd734f33897bce11f481b7aff6c8601daa8
Michael Hernández <michael.hernandez@grupobabel.com>
2022-07-14 14:38:54 -0600

    formulas

---
+++ SITEL/GB.SUTEL.UI/Scripts/Fonatel/JsRegistroIndicadores.js
@@ -0,0 +1,22 @@
+﻿jsRegistroIndicadorFonatel= {
+    "Controles": {
+        "btnllenadoweb": "#TableRegistroIndicadorFonatel tbody tr td .btn-home",
+
+    },
+    "Variables":{
+
+    },
+
+    "Metodos": {
+   
+    }
+
+}
+
+
+
+$(document).on("click", jsRegistroIndicadorFonatel.Controles.btnllenadoweb, function () {
+    let id = 1;
+    window.location.href = "/Fonatel/RegistroIndicadorFonatel/Create?id=" + id;
+});
+
