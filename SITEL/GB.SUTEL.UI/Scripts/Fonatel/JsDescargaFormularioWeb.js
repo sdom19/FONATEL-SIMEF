d684593593138751f80f4a5576e62e90f38c14fa
Michael Hernández <michael.hernandez@grupobabel.com>
2022-07-15 11:53:42 -0600

    Descarga de Formulario

---
+++ SITEL/GB.SUTEL.UI/Scripts/Fonatel/JsDescargaFormularioWeb.js
@@ -0,0 +1,28 @@
+﻿JsDescargaFormularioWeb= {
+    "Controles": {
+        "btndescarga": "#btnDescargaFormularioWeb",
+        "btnCancela": "#btnCancelaFormularioWeb"
+    },
+    "Variables": {
+   
+        
+    },
+
+    "Metodos": {
+   
+    }
+
+}
+
+
+
+$(document).on("click", JsDescargaFormularioWeb.Controles.btndescarga, function () {
+    debugger;
+    $("#divLoading").fadeIn();
+});
+
+
+$(document).on("click", JsDescargaFormularioWeb.Controles.btnCancela, function () {
+    window.location.href = "/DescargaFormulario/Create";
+});
+
