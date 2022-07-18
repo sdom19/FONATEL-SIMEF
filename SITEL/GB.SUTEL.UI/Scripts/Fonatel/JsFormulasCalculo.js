1c20b759a7a0b22aa32e517a47d64070783c7d71
Michael Hernández <michael.hernandez@grupobabel.com>
2022-07-18 11:13:55 -0600

    cambios formulas

---
+++ SITEL/GB.SUTEL.UI/Scripts/Fonatel/JsFormulasCalculo.js
@@ -0,0 +1,23 @@
+﻿JsFormulasCalculo= {
+    "Controles": {
+        "ddlDireccionFormula": "#ddlDireccionFormula",
+        "divDireccionFonatel": "div[name='divDireccionFonatel']"
+    },
+    "Variables": {
+        "Direccion": {
+            "FONATEL": 1,
+            "MERCADOS": 2,
+            "CALIDAD":3
+        }
+        
+    },
+
+    "Metodos": {
+   
+    }
+
+}
+
+$(document).on("change", JsFormulasCalculo.Controles.ddlDireccionFormula, function () {
+    alert("sdad");
+});
