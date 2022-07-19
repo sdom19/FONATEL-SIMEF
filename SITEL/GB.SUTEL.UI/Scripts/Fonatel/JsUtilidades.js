ed7004914198538472f449ca2c5049d4666f1d28
Michael Hernández <michael.hernandez@grupobabel.com>
2022-07-19 07:06:43 -0600

    Opción de Búsqueda en table

--- SITEL/GB.SUTEL.UI/Scripts/Fonatel/JsUtilidades.js
+++ SITEL/GB.SUTEL.UI/Scripts/Fonatel/JsUtilidades.js
@@ -24,7 +24,8 @@ $(document).ready(function () {
                 "next": "Siguiente",
                 "previous": "Anterior"
             }
-        }
+        },
+       
     });
 
 
@@ -78,7 +79,31 @@ $(document).ready(function () {
                 "next": "Siguiente",
                 "previous": "Anterior"
             }
-        }
+        },
+        initComplete: function () {
+            this.api()
+                .columns()
+                .every(function () {
+                    var column = this;
+
+                    if ($(column.footer()).hasClass("mostrar_fotter")) {
+                        var select = $('<select class="form-control" ><option value="">Seleccione</option></select>')
+                            .appendTo($(column.footer()).empty())
+                            .on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                                column.search(val ? '^' + val + '$' : '', true, false).draw();
+                            });
+
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function (d, j) {
+                                select.append('<option value="' + d + '">' + d + '</option>');
+                            });
+                    }
+                });
+        },
     });
     $('.listasDesplegables').select2({
         placeholder: "Seleccione",
