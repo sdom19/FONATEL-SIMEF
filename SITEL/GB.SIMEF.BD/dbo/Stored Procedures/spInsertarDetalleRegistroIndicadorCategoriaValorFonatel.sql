d6e321059dca1c6c94a80f8f99cb57f65693388b
georgimesen <georgi.mesen@grupobabel.com>
2022-11-28 16:19:35 -0600

    Scripts de Registro indicador

---
+++ SITEL/GB.SIMEF.BD/dbo/Stored Procedures/spInsertarDetalleRegistroIndicadorCategoriaValorFonatel.sql
@@ -0,0 +1,22 @@
+﻿CREATE procedure [dbo].[spInsertarDetalleRegistroIndicadorCategoriaValorFonatel]
+(
+	   @lst TypeDetalleRegistroIndicadorCategoriaValorFonatel READONLY
+)
+AS
+BEGIN TRY
+
+INSERT INTO SITELP.FONATEL.DetalleRegistroIndicadorCategoriaValorFonatel(IdSolicitud,IdFormulario,IdIndicador,idCategoria,NumeroFila,Valor)
+SELECT IdSolicitud,IdFormulario,IdIndicador,idCategoria,NumeroFila,Valor FROM @lst;
+
+SELECT IdSolicitud,IdFormulario,IdIndicador,idCategoria,NumeroFila,Valor FROM SITELP.FONATEL.DetalleRegistroIndicadorCategoriaValorFonatel;
+
+END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		BEGIN
+			SELECT   
+				ERROR_NUMBER() AS ErrorNumber  
+			   ,ERROR_MESSAGE() AS ErrorMessage; 
+			ROLLBACK TRANSACTION;
+		END
+END CATCH		
---
+++ SITEL/GB.SIMEF.BD/dbo/Tables/TypeDetalleRegistroIndicadorCategoriaValorFonatel.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [TypeDetalleRegistroIndicadorCategoriaValorFonatel] AS TABLE(
+	[IdSolicitud] [int] NOT NULL,
+	[IdFormulario] [int] NOT NULL,
+	[IdIndicador] [int] NOT NULL,
+	[idCategoria] [int] NOT NULL,
+	[NumeroFila] [int] NOT NULL,
+	[Valor] [varchar](1000) NOT NULL,
+ PRIMARY KEY([IdSolicitud],[IdFormulario],[IdIndicador],[idCategoria],[NumeroFila])
+)
