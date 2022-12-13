654ea4a34ea9561fa7b44f1aa970c8fe0cdaee52
georgimesen <georgi.mesen@grupobabel.com>
2022-12-13 11:26:27 -0600

    Cambio en API para obtener tablas para el proceso de Power BI

---
+++ SITEL/GB.SIMEF.API/Model/InformeEncabezadoTabla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GB.SIMEF.API.Model
+{
+    [Table("InformeEncabezadoTabla", Schema = "FONATEL")]
+    public class InformeEncabezadoTabla
+    {
+        public InformeEncabezadoTabla()
+        {
+
+        }
+
+        [Key]
+        public int IdTabla { get; set; }
+        public int IdEncabezado { get; set; }
+        public string NombreEncabezado { get; set; }
+    }
+}
---
+++ SITEL/GB.SIMEF.API/Model/InformeFilasValor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GB.SIMEF.API.Model
+{
+    [Table("InformeFilasValor", Schema = "FONATEL")]
+    public class InformeFilasValor
+    {
+        public InformeFilasValor()
+        {
+
+        }
+
+        [Key]
+        public int IdTabla { get; set; }
+        public int IdEncabezado { get; set; }
+        public int IdFilaValor { get; set; }
+        public string Valor { get; set; }
+    }
+}
