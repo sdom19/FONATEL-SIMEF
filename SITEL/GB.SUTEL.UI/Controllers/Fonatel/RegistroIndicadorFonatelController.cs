31fd9e569a6b491c5085110cc78e2a10c422100b
georgimesen <georgi.mesen@grupobabel.com>
2022-12-20 10:09:32 -0600

    Avance sobre guardar multiple registro indicador

--- SITEL/GB.SUTEL.UI/Controllers/Fonatel/RegistroIndicadorFonatelController.cs
+++ SITEL/GB.SUTEL.UI/Controllers/Fonatel/RegistroIndicadorFonatelController.cs
@@ -252,7 +252,7 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
         }
 
         [HttpPost]
-        public async Task<string> ActualizarDetalleRegistroIndicadorFonatel(DetalleRegistroIndicadorFonatel obj)
+        public async Task<string> ActualizarDetalleRegistroIndicadorFonatel(List<DetalleRegistroIndicadorFonatel> listaActDetalleRegistroIndicador)
         {
 
             //Creamos una variable resultado de tipo lista DetalleRegistroIndicadorFonatel
@@ -261,7 +261,7 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
             await Task.Run(() =>
             {
                 //Conectamos con el BL de relacion categoria para insertar y enviamos  la relacion
-                result = detalleRegistroIndicadorBL.ActualizarElemento(obj);
+                result = detalleRegistroIndicadorBL.ActualizarDetalleRegistroIndicadorFonatelMultiple(listaActDetalleRegistroIndicador);
             });
 
             //Retornamos un Json con el resultado
