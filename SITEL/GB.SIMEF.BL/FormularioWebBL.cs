29ab09ccdd90688458e1f9d6ef1c74de9657c447
georgimesen <georgi.mesen@grupobabel.com>
2023-03-02 15:17:56 -0600

    Correcion bug 88388

--- SITEL/GB.SIMEF.BL/FormularioWebBL.cs
+++ SITEL/GB.SIMEF.BL/FormularioWebBL.cs
@@ -97,7 +97,8 @@ namespace GB.SIMEF.BL
         private int ValidarEstado(FormularioWeb obj)
         {
             FormularioWeb formularioWebViejo = clsDatos.ObtenerDatos(obj).Single();
-            if (obj.Descripcion == null || obj.Descripcion == "" || obj.CantidadIndicadores == 0 || obj.idFrecuencia == 0 || obj.CantidadIndicadores != formularioWebViejo.CantidadIndicadores)
+            if (obj.Descripcion == null || obj.Descripcion == "" || obj.CantidadIndicadores == 0 || obj.idFrecuencia == 0 
+                || obj.CantidadIndicadores != formularioWebViejo.CantidadIndicadores || formularioWebViejo.idEstado == (int)Constantes.EstadosRegistro.EnProceso)
             { 
                 if (formularioWebViejo.EstadoRegistro.idEstado == (int)Constantes.EstadosRegistro.Desactivado)
                 { 
