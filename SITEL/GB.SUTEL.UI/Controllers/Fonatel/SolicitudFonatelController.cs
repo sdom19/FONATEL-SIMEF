18e7dd3af8d7dba64f9d3582c35b40913b107edd
Adolfo Cunquero <adolfo.cunquero@grupobabel.com>
2023-03-08 16:59:50 -0600

    Solicitudes, correccion bug 88767

--- SITEL/GB.SUTEL.UI/Controllers/Fonatel/SolicitudFonatelController.cs
+++ SITEL/GB.SUTEL.UI/Controllers/Fonatel/SolicitudFonatelController.cs
@@ -79,7 +79,7 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
             ViewBag.ListaAnno = AnnoBL.ObtenerDatos(new Anno() ).objetoRespuesta;
             ViewBag.Modo = modo.ToString();
             ViewBag.ListaMes= MesBL.ObtenerDatos(new Mes()).objetoRespuesta;
-            ViewBag.ListaFuentes = fuenteBl.ObtenerDatos(new FuentesRegistro()).objetoRespuesta;
+            ViewBag.ListaFuentes = fuenteBl.ObtenerDatos(new FuentesRegistro() { idEstado = (int)Constantes.EstadosRegistro.Activo }).objetoRespuesta;
 
 
             ViewBag.ListaFormularioWeb = formularioWebBL.ObtenerDatos(new FormularioWeb() {idEstado=(int)Constantes.EstadosRegistro.Activo })
