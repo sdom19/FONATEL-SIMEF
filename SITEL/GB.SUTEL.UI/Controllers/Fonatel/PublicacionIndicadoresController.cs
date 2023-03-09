7ed06fe8c8c138bb3018e6cbf0314368b1ebcc86
Adolfo Cunquero <adolfo.cunquero@grupobabel.com>
2023-03-09 13:14:49 -0600

    Publicacion indicadores, correccion bug 88806

--- SITEL/GB.SUTEL.UI/Controllers/Fonatel/PublicacionIndicadoresController.cs
+++ SITEL/GB.SUTEL.UI/Controllers/Fonatel/PublicacionIndicadoresController.cs
@@ -20,6 +20,7 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
     {
 
         private readonly IndicadorFonatelBL indicadorfonatelBL;
+        private readonly DefinicionIndicadorBL definicionBL;
 
         // GET: CategoriasDesagregacion
 
@@ -27,6 +28,7 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
         public PublicacionIndicadoresController()
         {
             indicadorfonatelBL = new IndicadorFonatelBL(EtiquetasViewPublicaciones.PublicacionIndicadores, System.Web.HttpContext.Current.User.Identity.GetUserId());
+            definicionBL = new DefinicionIndicadorBL(EtiquetasViewDefinicionIndicadores.TituloIndex, System.Web.HttpContext.Current.User.Identity.GetUserId());
         }
 
 
@@ -57,9 +59,11 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
             RespuestaConsulta<List<Indicador>> result = null;
             await Task.Run(() =>
             {
+                var def = definicionBL.ObtenerDatos(new DefinicionIndicador() { idEstado = (int)Constantes.EstadosRegistro.Activo });
                 result = indicadorfonatelBL.ObtenerDatos(new Indicador() { idEstado = (int)Constantes.EstadosRegistro.Activo });
                 result.objetoRespuesta = result
-                    .objetoRespuesta.Where(x => x.IdClasificacion != (int)Constantes.ClasificacionIndicadorEnum.Entrada).ToList();
+                    .objetoRespuesta.Where(x => x.IdClasificacion != (int)Constantes.ClasificacionIndicadorEnum.Entrada && def.objetoRespuesta.Any(d => d.idIndicador == x.idIndicador)).ToList();
+
             });
             return JsonConvert.SerializeObject(result);
         }
@@ -86,12 +90,11 @@ namespace GB.SUTEL.UI.Controllers.Fonatel
                 return indicadorfonatelBL.ObtenerDatos(indicador);
 
             }).ContinueWith(data =>
-             {
+            {
                 Indicador objetoActualizar = data.Result.objetoRespuesta.Single();
                 objetoActualizar.VisualizaSigitel = indicador.VisualizaSigitel;
                 result = indicadorfonatelBL.PublicacionSigitel(objetoActualizar);
-             }
-            );
+            });
             return JsonConvert.SerializeObject(result);
         }
 
