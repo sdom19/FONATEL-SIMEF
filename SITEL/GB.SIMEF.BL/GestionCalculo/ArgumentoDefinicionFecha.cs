c9278cdbdca2342a562f276767897203222686a9
José Navarro Acuña <jose.navarro@grupobabel.com>
2023-04-03 09:22:59 -0600

    definicion fecha

--- SITEL/GB.SIMEF.BL/GestionCalculo/ArgumentoDefinicionFecha.cs
+++ SITEL/GB.SIMEF.BL/GestionCalculo/ArgumentoDefinicionFecha.cs
@@ -1,5 +1,6 @@
 ﻿using GB.SIMEF.Entities;
 using System;
+using System.Globalization;
 
 namespace GB.SIMEF.BL.GestionCalculo
 {
@@ -15,20 +16,36 @@ namespace GB.SIMEF.BL.GestionCalculo
         {
             FormulaDefinicionFecha argumentoFecha = (FormulaDefinicionFecha)pArgumentoFormula;
 
+            DateTime fechaInicio = (DateTime)(argumentoFecha.FechaInicio != null ? argumentoFecha.FechaInicio : DateTime.MinValue);
+            DateTime fechaFinal = (DateTime)(argumentoFecha.FechaFinal != null ? argumentoFecha.FechaFinal : DateTime.MinValue);
+
             return string.Format(
-                "EXEC pa_ConstruirArgumentoSalidaFecha {0}, {1}, {2}, {3}, {4}, {5}, {6}",
+                "EXEC pa_ConstruirArgumentoSalidaFecha {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}",
                 argumentoFecha.IdUnidadMedida,
                 
                 argumentoFecha.IdTipoFechaInicio,
-                argumentoFecha.FechaInicio != null ? argumentoFecha.FechaInicio.ToString() : DateTime.MinValue.ToString(),
+                ConvertirFechaAFormatoSQL(fechaInicio),
                 argumentoFecha.IdCategoriaDesagregacionInicio != null ? argumentoFecha.IdCategoriaDesagregacionInicio : 0,
                 
                 argumentoFecha.IdTipoFechaFinal,
-                argumentoFecha.FechaFinal != null ? argumentoFecha.FechaFinal.ToString() : DateTime.MinValue.ToString(),
+                ConvertirFechaAFormatoSQL(fechaFinal),
                 argumentoFecha.IdCategoriaDesagregacionFinal != null ? argumentoFecha.IdCategoriaDesagregacionFinal : 0,
 
-                argumentoFecha.IdIndicador
+                argumentoFecha.IdIndicador,
+
+                pFormulasCalculo.IdIndicador
             );
+        }
+
+        /// <summary>
+        /// 31/03/2023
+        /// José Navarro Acuña
+        /// Función que permite convertir una fecha a un compatible con SQL
+        /// </summary>
+        /// <returns></returns>
+        private string ConvertirFechaAFormatoSQL(DateTime pFecha)
+        {
+            return "'" + pFecha.ToString("MM/dd/yyyy") + "'";
         }
     }
 }
